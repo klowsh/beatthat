02c64a9ef49b596e27e13d285b4110cb01bfeb48
klowsh <06_inane_dynamic@icloud.com>
2022-06-15 21:10:20 +0800

    final version for submission

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
 /* Project 2: Beat That
+
+How many hours did you spend on this assignment?: 15 hours (at least 4-5 nights)
+
+What part of the assignment did you spend the most time on?: 
+- Initially, the part that took the most time was deciding on the logic to use for multiple players, and using "nested arrays" (or arrays within an array).
+- After this part was completed/coded, I had a lot of trouble deciding on how to get the results for tied players (for example, more than a single player who got the same high score)
+
+How comfortable did you feel with this assignment? (1-5): About 2, with 5 being most comfortable
+
+Is there anything in this code that you feel pleased about?: 
+I was glad that I learnt how to Google and find online resources for the code. For example, I am happy that I managed to use new syntax like .splice
+
+What's one aspect of your code you would like specific, elaborate feedback on?:
+- Similar to the previous code, I found difficulty in deciding what is the proper way to get around coding -- i.e.: deciding on the logic to implement the code. Would appreciate advice on this! And how do seasoned / more experienced programmers decide on a logic that works and is easy to implement.
+- I also am finding it difficult to refactor my code to make there more helper functions. I am afraid if I do remove parts of the code, the "nested arrays" part of it would not be able to work.
+
  */
 
 // Declare global variables
 var currentGameMode = "input number of players";
-
 let diceLogs = [
   [0, 0],
   [1, 1],
 ];
-
 let diceCombinedLogs = [];
-
 var playerNumber = 0;
 var numberOfPlayers;
-
-// For double arrays, index = player position (rows) and columns = dice rolled
-
-// Player picks order for the dice they want - can use array to
-// Highest combined number wins - can use array to iterate through and get combined number
 
 var main = function (input) {
   var myOutputValue = "";
   var numberOfDiceRolls = 2;
-  console.table(diceLogs);
 
-  // Game Mode 1: To set number of players
+  // GAME MODE 1: To set number of players
   if (currentGameMode == "input number of players") {
     // Input validation, to accept maximum 10 players
-    if (input > 0 && input <= 10) {
+    if (input > 1 && input <= 10) {
       numberOfPlayers = Math.ceil(input); // To get an integer, do not accept real numbers as players
-      console.log("Number of players is " + numberOfPlayers);
-
       myOutputValue = `You have selected ${numberOfPlayers} players. <br> <br> It is Player 1's turn. Player 1, please press submit to roll the dice.`;
       currentGameMode = "start to roll dice";
     } else {
-      myOutputValue = `Please enter a valid number of players between 1 to 10 (inclusive). Thank you.`;
+      myOutputValue = `Please enter a valid number of players between 2 to 10 (inclusive). Thank you.`;
     }
     return myOutputValue;
   }
 
-  // Game Mode 2: Roll dice
+  // GAME MODE 2: Roll dice
   if (currentGameMode == "start to roll dice") {
-    console.log("Number of players is " + numberOfPlayers);
-    console.log("Player number is " + playerNumber);
+    console.log("Player number is " + (playerNumber + 1));
     if (playerNumber == 0) {
       diceLogs.splice(playerNumber, 0, [rollDice(), rollDice()]); // Insert at top of the table
       diceLogs.pop(); // Remove initial variables that were added to make the table
@@ -50,43 +54,57 @@ var main = function (input) {
     } else {
       diceLogs.push([rollDice(), rollDice()]); // Insert at the bottom of the table
     }
-    console.table(diceLogs);
+    console.table(diceLogs); // To see the original dice roll
 
-    myOutputValue = `Player ${playerNumber + 1}: 🎲
+    // Typically, will proceed to ask to choose position
+
+    // However, if Dice 1 and Dice 2 are the same, do not proceed to ask to choose position
+    if (diceLogs[playerNumber][0] == diceLogs[playerNumber][1]) {
+      // to continue game if not all players have played yet
+      if (playerNumber + 1 == numberOfPlayers) {
+        var combinedDiceNumber = Number(
+          diceLogs[playerNumber][0] * 10 + diceLogs[playerNumber][1]
+        );
+        diceCombinedLogs.push(combinedDiceNumber);
+        myOutputValue = `Player ${
+          playerNumber + 1
+        }, both dice returned the same value. Your number is ${combinedDiceNumber}. <br> <br> Press submit for next player's turn, or to see results if all players have rolled the dice.`;
+
+        currentGameMode = "get winner";
+        return myOutputValue;
+      } else {
+        var combinedDiceNumber = Number(
+          diceLogs[playerNumber][0] * 10 + diceLogs[playerNumber][1]
+        );
+        diceCombinedLogs.push(combinedDiceNumber);
+
+        myOutputValue = `Player ${
+          playerNumber + 1
+        }, both dice returned the same value. Your number is ${combinedDiceNumber}. <br> <br> Press submit for next player's turn, or to see results if all players have rolled the dice.`;
+
+        playerNumber += 1;
+      }
+    }
+
+    // If not the same dice roll, to proceed to NEXT game mode to ask to choose position
+    else {
+      myOutputValue = `Player ${playerNumber + 1}: 🎲
       <br> <br> You have rolled ${
         diceLogs[playerNumber][0]
       } for your Dice 1, and ${
-      diceLogs[playerNumber][1]
-    } for your Dice 2. <br> <br> Please choose if Dice 1 or Dice 2 should go first. <br> <br> Input either 1 or 2 only.`;
+        diceLogs[playerNumber][1]
+      } for your Dice 2. <br> <br> Please choose if Dice 1 or Dice 2 should go first. <br> <br> Input either 1 or 2 only.`;
 
-    // Do not ask to choose position if Dice 1 and Dice 2 are the same
-    // if (diceLogs[playerNumber][0] == diceLogs[playerNumber][1]) {
-    //   console.log(diceLogs[playerNumber][0]);
-    //   console.log(diceLogs[playerNumber][1]);
-    //   console.log("C");
-    //   var combinedDiceNumber = Number(
-    //     diceLogs[playerNumber][0] * 10 + diceLogs[playerNumber][1]
-    //   );
-    //   diceCombinedLogs.push(combinedDiceNumber);
-    //   console.log(diceCombinedLogs);
-    //   myOutputValue = `Both dice returned the same value. Your number is ${combinedDiceNumber}.`;
-    //   playerNumber += 1;
-    //   currentGameMode = "start to roll dice";
+      // Change game mode to Game Mode 3
+      currentGameMode = "choose position of dice";
+    }
 
-    //   return myOutputValue;
-    // }
-
-    // Change game mode
-    currentGameMode = "choose position of dice";
-    console.log("currentGameMode is " + currentGameMode);
-
-    // Game Mode 3: For player to choose position of dice
+    // GAME MODE 3: For player to choose position of dice
   } else if (currentGameMode == "choose position of dice") {
     // Player wants to swap the position
     if (Number(input) == 2) {
-      console.log("Player number is " + playerNumber);
       swapPositions(diceLogs, playerNumber, 0, 1);
-      console.table(diceLogs);
+
       var combinedDiceNumber = Number(
         diceLogs[playerNumber][0] * 10 + diceLogs[playerNumber][1]
       );
@@ -94,7 +112,10 @@ var main = function (input) {
       myOutputValue = `Player ${
         playerNumber + 1
       }, you have chosen Dice 2 to go first. Your number is ${combinedDiceNumber}. <br> <br> Press submit for next player's turn, or to see results if all players have rolled the dice.`;
-    } else if (Number(input) == 1) {
+    }
+
+    // Player does not want to swap the position
+    else if (Number(input) == 1) {
       var combinedDiceNumber = Number(
         diceLogs[playerNumber][0] * 10 + diceLogs[playerNumber][1]
       );
@@ -108,46 +129,52 @@ var main = function (input) {
       myOutputValue = `Invalid entry. Please enter only either Dice 1 or 2 to go first.  <br> <br> Press submit for next player's turn, or to see results if all players have rolled the dice.`;
       return myOutputValue;
     }
-    console.log(diceCombinedLogs);
 
     // Stop the loop once all players have played, proceed to find the winner
     if (playerNumber + 1 == numberOfPlayers) {
-      console.log("A");
-      var winnerPlayer = largestNumber(diceCombinedLogs) + 1; // Need to +1 as array starts from 0
-      console.log("The winner player is " + winnerPlayer);
-      myOutputValue = `The winner is Player ${winnerPlayer}, with the winning number of ${
-        diceCombinedLogs[winnerPlayer - 1]
-      }.`;
-      return myOutputValue;
+      currentGameMode = "get winner";
+      myOutputValue = `Please press submit to get the winner.`;
     } else {
       // Increase player number
       playerNumber += 1;
-      console.log("Player number is " + playerNumber);
 
       currentGameMode = "start to roll dice"; // Return to Game Mode 2 for next player
     }
   }
+
+  // GAME MODE 4: Get winner mode
+  else if (currentGameMode == "get winner") {
+    var winnerPlayer = getWinners(diceCombinedLogs); // Need to +1 as array starts from 0
+    myOutputValue = `The winner(s) is/are Player(s) ${winnerPlayer}, with the winning number of ${Math.max(
+      ...diceCombinedLogs
+    )}.`;
+    return myOutputValue;
+  }
+
   return myOutputValue;
 };
 
-// Defince function to find the largest number in an array
-var largestNumber = function (arr) {
+// Define function to find the largest number in an array
+var getWinners = function (arr) {
   var largestNo = arr[0];
-  var j = 0;
-  for (var i = 0; i < arr.length; i += 1) {
-    if (arr[i] > largestNo) {
-      largestNo = arr[i];
-      j = i;
+  var arrOfLargestNumber = [];
+  for (var index = 0; index < arr.length; index += 1) {
+    if (arr[index] > largestNo) {
+      largestNo = arr[index];
+      arrOfLargestNumber = []; // to reset the array if there is a number larger than previous numbers, not able to use pop as there could be more than 1 player with a tied number
+      arrOfLargestNumber.push(index);
+    } else if (arr[index] == largestNo) {
+      arrOfLargestNumber.push(index); // if tie to push into array
     }
   }
-  return j;
+  var listOfWinners = arrOfLargestNumber.map((x) => x + 1);
+  return listOfWinners;
 };
-// Potential area to work on: how to return if there is the tie
 
 // Define function to roll dice
 var rollDice = function () {
   var randomDice = Math.ceil(Math.random() * 6);
-  return 1;
+  return randomDice;
 };
 
 // Define function to swap positions in array
